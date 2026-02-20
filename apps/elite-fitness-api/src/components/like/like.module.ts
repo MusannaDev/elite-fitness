4f1b43c1e4ec496e446029f90a060fa405f4b59c
MusannaDev <juraevotabek2002@gmail.com>
2026-02-20 17:32:58 +0900

    BRR: initial setups

---
+++ apps/elite-fitness-api/src/components/like/like.module.ts
@@ -0,0 +1,17 @@
+import { Module } from '@nestjs/common';
+import { LikeService } from './like.service';
+import { MongooseModule } from '@nestjs/mongoose';
+import LikeSchema from '../../schemas/Like.model';
+import { AuthModule } from '../auth/auth.module';
+import { ViewModule } from '../view/view.module';
+import { MemberModule } from '../member/member.module';
+import { BoardArticleModule } from '../board-article/board-article.module';
+
+@Module({
+  imports: [
+    MongooseModule.forFeature([{name: "Like", schema: LikeSchema}]),
+  ],
+  providers: [LikeService],
+  exports: [LikeService]
+})
+export class LikeModule {}
